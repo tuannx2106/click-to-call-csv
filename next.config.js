63508e245acac3c091d0721e118ed260ebb274cd
tuannx <xuantuan.nguyen@shopee.com>
2021-08-24 23:48:38 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,20 +2,14 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withAntdLess = require('next-plugin-antd-less');
 
+const subPath = '/click-to-call-csv';
+
 module.exports = {
   ...withAntdLess({
-    // optional
-    // optional
     lessVarsFilePath: './src/styles/variables.less',
-    // optional
     lessVarsFilePathAppendToEndOfContent: false,
-    // optional https://github.com/webpack-contrib/css-loader#object
     cssLoaderOptions: {},
-
-    // Other Config Here...
-
-    webpack(config) {
-      return config;
-    },
   }),
+  basePath: subPath,
+  assetPrefix: subPath,
 };
